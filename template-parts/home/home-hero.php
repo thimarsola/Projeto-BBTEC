918d59ee360705fc477e4a3b1fac849e7a23b709
Marsola <tmarsola94@gmail.com>
2022-03-04 09:14:28 -0300

    Finish the hero section in homepage

--- template-parts/home/home-hero.php
+++ template-parts/home/home-hero.php
@@ -6,15 +6,15 @@
 
              <!-- header -->
               <header class="hero__container__header mb-3">
-                  <h2 class="f-7 clr-white-500">Contamos com linhas completas de peças sob medida!</h2>
+                  <h2 class="f-7 clr-white-500">Energizando sua obra com economia e praticidade</h2>
               </header>
              <!-- end of header -->
 
              <!-- text -->
               <div class="hero__container__text clr-white-500">
-                  <p class="mb-7">Produção e comercialização de peças e partes para diversos setores da indústria: Automotivo, Eletroeletrônico, Energia Convencional, Eólica e Solar, Ferroviário, Hospitalar, Mecânica, Siderurgia, entre outros.</p>
+                  <p class="mb-7">Flexibilidade e agilidade nas instalações e alterações de projetos elétricos.</p>
 
-                  <a href="#sobre-nos" class="btn btn-primary-500 radius" title="Conheça mais sobre as Indústrias RC">Saiba mais</a>
+                  <a href="#sobre-nos" class="btn btn-primary-500 radius" title="Conheça mais sobre a <?= SITE['name'] ?>">Saiba mais</a>
               </div>
              <!-- end of text -->
 
