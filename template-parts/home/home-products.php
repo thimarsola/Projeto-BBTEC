c1109a17352b9064019c0362c39c89b2ad749e46
Marsola <tmarsola94@gmail.com>
2022-03-04 10:19:20 -0300

    Finish the products section in home page

--- template-parts/home/home-products.php
+++ template-parts/home/home-products.php
@@ -1,4 +1,4 @@
-<section class="products py-s <?= (is_home() ? 'bg-light-blue-500' : ''); ?>">
+<section class="products py-s <?= (is_home() ? 'bg-light-gray-500' : ''); ?>">
     <div class="container">
 
         <!-- header -->
@@ -6,40 +6,64 @@
         <!-- end of header -->
 
         <!-- row -->
-         <div class="products__row grid">
-             <?php
-             $jsonProducts = file_get_contents(__DIR__ . '/../../includes/products.json');
-             $productsList = json_decode($jsonProducts, true);
+        <div class="products__row grid">
+            <?php
+            $jsonProducts = file_get_contents(__DIR__ . '/../../includes/products.json');
+            $productsList = json_decode($jsonProducts, true);
 
-             foreach ($productsList['products'] as $product):
-             ?>
+            foreach ($productsList['products'] as $product):
+                ?>
 
-             <!-- card -->
-              <article class="products__row__card bg-white-500 shadow">
-                  <!-- icon -->
-                   <div class="products__row__card__icon t-center">
-                       <i class="icon-<?= $product['icon']; ?> clr-primary-500 f-8"></i>
-                   </div>
-                  <!-- end of icon -->
+                <!-- card -->
+                <article class="products__row__card bg-white-500 shadow">
+                    <!-- image -->
+                    <div class="products__row__card__image <?= ($product['release'] == true ? 'bg-release-500': ''); ?>">
+                        <?php
 
-                  <!-- header -->
-                   <header class="products__row__card__header mt-3 mb-2 t-center">
-                       <h3><?= $product['title']; ?></h3>
-                   </header>
-                  <!-- end of header -->
+                        if(!$product['image'] == false){
+                            echo picture('imagem-' . $product['image'], 'Conheça essa nossa linha de produtos - ' . $product['title'], 'img-fluid');
+                        }
 
-                  <!-- body -->
-                   <div class="products__row__card__body t-center">
-                       <p class="t-small mb-4">Clique no botão abaixo para maiores informações sobre essa linha de produtos</p>
+                        if ($product['release'] == true):
+                            ?>
 
-                       <a class="btn btn-primary-500 radius" href="<?= get_the_permalink(get_page_by_title($product['title'])); ?>" title="Conheça essa nossa linha de produto" target="_blank">Saiba mais</a>
-                   </div>
-                  <!-- end of body -->
-              </article>
-             <!-- end of card -->
+                            <!-- release -->
+                            <div class="products__row__card__image__release bg-primary-500 clr-white-500 p-1 t-small">
+                                <p class="t-uppercase">
+                                    <b>Breve lançamento</b>
+                                </p>
+                            </div>
+                            <!-- end of release -->
 
-             <?php endforeach; ?>
-         </div>
+                        <?php endif; ?>
+                    </div>
+                    <!-- end of image -->
+                    
+                    <!-- content -->
+                     <div class="products__row__card__content">
+                         <!-- header -->
+                         <header class="products__row__card__content__header mt-3 mb-2 t-center">
+                             <h3><?= $product['title']; ?></h3>
+                             <h4>(<?= $product['subtitle']; ?>)</h4>
+                         </header>
+                         <!-- end of header -->
+
+                         <?php if ($product['release'] == false): ?>
+                             <!-- body -->
+                             <div class="products__row__card__content__body t-center">
+                                 <p class="t-small mb-4">Clique no botão abaixo para maiores informações sobre essa linha de produtos</p>
+
+                                 <a class="btn btn-primary-500 radius" href="<?= get_the_permalink(get_page_by_title($product['link'])); ?>" title="Conheça essa nossa linha de produto" target="_blank">Saiba mais</a>
+                             </div>
+                             <!-- end of body -->
+                         <?php endif; ?>
+                     </div> 
+                    <!-- end of content -->
+                </article>
+                <!-- end of card -->
+
+            <?php endforeach; ?>
+        </div>
         <!-- end of row -->
 
     </div>
